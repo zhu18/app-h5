24ec484bf1e0ede905b7edf636b1c46da1ec92fe
jus-pro <ws@jusfoun.com>
2019-04-15 11:40:05 +0800

    新闻首页

--- src/views/news/index.vue
+++ src/views/news/index.vue
@@ -8,32 +8,33 @@
   <div class="news-wrap">
     <!-- 头部 开始 -->
     <div class="top-header">
-      
+      <span class="iconfont icon-back" @click='back'></span>
+      <span class="title">新闻资讯</span>
+      <span class="iconfont icon-txtquery"></span>
     </div>
     <!-- 头部 结束 -->
  
     <!-- 新闻列表  开始  -->
-    <div class="news-list">
-            <div
-            v-infinite-scroll="loadMore"
-            infinite-scroll-disabled="loading"
-            infinite-scroll-distance="10">
-            <template v-for="item in list" >
-                <router-link class="list-item" tag="div" to="/detailsInfo" >
-                    <div class="img-box">
-                        <img src="../../assets/images/home/testlogo.jpg" alt="">
+    <div class="news-list"
+        v-infinite-scroll="loadMore"
+        infinite-scroll-disabled="loading"
+        infinite-scroll-distance="10">
+        <template v-for="(item,index) in list" >
+            <router-link class="list-item" tag="div" to="/newsdetails" :key="index">
+                <div class="img-box" v-if="item.images.length==1">
+                    <img src="../../assets/images/news/news.png" alt="" v-for="(url,index2) in item.images" :key="index2">
+                </div>
+                <div class="item-content">
+                    <h4>{{item.title}}</h4>
+                    <div class="img-box2" v-if="item.images.length>1">
+                        <img src="../../assets/images/news/news.png" alt="" v-for="(url,index2) in item.images" :key="index2">
                     </div>
-                    <div class="item-content">
-                        <h4>耐克创新有限合伙公司</h4>
-                        <p><span class="title">注册号</span> <span>{{item}}</span></p>
-                        <p><span class="title">申请日</span> <span>12749588</span></p>
-                        <p><span class="title">当前状态</span> <span class="icon">商标无效</span></p>
-                    </div>
-                </router-link>
-            </template>
-            <div class="loading" v-if="loading">
-               <span class="tips">数据加载中</span><mt-spinner :type="3" color="#26a2ff"></mt-spinner>
-            </div>
+                    <div class="tinfo"><span class="title">{{item.source}}<var>{{item.numbers}}评论</var></span> <span>{{item.times}}</span></div>
+                </div>
+            </router-link>
+        </template>
+        <div class="loading" v-if="loading">
+            <span class="tips">数据加载中</span><mt-spinner :type="3" color="#26a2ff"></mt-spinner>
         </div>
     </div>
     <!-- 新闻列表  结束  -->
@@ -47,7 +48,48 @@ export default {
   data() {
     return {
         loading:false,
-        list:[1,2,3,4]
+        list:[
+            {
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:['../../assets/images/news/news.png']
+            },
+             {
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:[]
+            },{
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:['../../assets/images/news/news.png','../../assets/images/news/news.png','../../assets/images/news/news.png']
+            },{
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:['../../assets/images/news/news.png']
+            },
+             {
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:[]
+            },{
+                title:'在海外习近平听孩子们唱诵过这些中文诗、中文歌',
+                source:'新华网',
+                numbers:'2223',
+                times:'17分钟前',
+                images:['../../assets/images/news/news.png','../../assets/images/news/news.png','../../assets/images/news/news.png']
+            }
+
+        ]
     };
   },
   created() {},
@@ -61,8 +103,8 @@ export default {
         this.loading = true;
         setTimeout(() => {
             let last = this.list[this.list.length - 1];
-            for (let i = 1; i <= 4; i++) {
-            this.list.push(last + i);
+            for (let i = 1; i <= 6; i++) {
+            this.list.push(last);
             }
             this.loading = false;
         }, 3500);
@@ -79,12 +121,94 @@ export default {
 <style lang="scss" scoped>
 ::-webkit-search-cancel-button { display: none; }
 .news-wrap{
+    .top-header{
+        height: 0.9rem;
+        background-color: #2095f2;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #fff;
+        box-shadow: 0 1px 3px rgba(0,0,0,.1);
+        .iconfont{
+            font-size: 0.84rem;
+            color: #fff;
+        }
+        .title{
+            font-size: 0.36rem;
+        }
+    }
     position:absolute;
     top:0;
     left:0;
     right:0;
     bottom:1.28rem;
-    background-color: #f6f6f6;
+    // background-color: #f6f6f6;
+    .news-list{
+        height: calc(100% - 0.9rem);
+        overflow-y: auto;
+        padding: 0.32rem;
+        // padding-bottom: 0.9rem;
+        box-sizing: border-box;
+        .list-item{
+            padding: 0.32rem 0;
+            display: flex;
+            background: #fff;
+            // box-shadow: 0 1px 3px rgba(0,0,0,.1);
+            border-bottom:1px solid rgba(0,0,0,.1);
+            .img-box{
+                width: 2.2rem;
+                height: 1.2rem;
+                margin-right: 0.2rem;
+                img{
+                  width: 2.2rem;
+                  height: 1.2rem;
+                  border-radius: 0.1rem;
+                }
+            }
+            .img-box2{
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+                padding:0.32rem 0;
+                img{
+                  width: 31%;
+                  height: 1.2rem;
+                  border-radius: 0.1rem;
+                }
+            }
+            .item-content{
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                min-height: 1.2rem;
+                flex: 1;
+                h4{
+                    font-size: 0.28rem;
+                    color: #1e2128;
+                    font-weight: normal;
+                    white-space: wrap;
+                    line-height: 0.32rem;
+                }
+                .tinfo{
+                    font-size: 0.24rem;
+                    color: #bfbfbf;
+                    display: flex;
+                    justify-content: space-between;
+                    align-content: center;
+                    var{
+                        font-size: 0.2rem;
+                        color: #fea71a;
+                        font-style: normal;
+                        border: 1px solid #fea71a;
+                        padding: 0.1rem 0.2rem;
+                        border-radius: 0.2rem;
+                        margin-left:0.2rem;
+                    }
+                }
+
+            }
+        }
+    }
 }
 
 
@@ -92,7 +216,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 0.6rem;
+    height: 0.9rem;
     .tips{
         margin:0 0.3rem;
         display: inline-block;
