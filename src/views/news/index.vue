abff421b59d186d2b0e9944ed6595a234ba231f2
朱润亚 <zhu18@vip.qq.com>
2019-04-16 10:12:35 +0800

    。

--- src/views/news/index.vue
+++ src/views/news/index.vue
@@ -152,7 +152,7 @@ export default {
         .list-item{
             padding: 0.32rem 0;
             display: flex;
-            background: #fff;
+            // background: #fff;
             // box-shadow: 0 1px 3px rgba(0,0,0,.1);
             border-bottom:1px solid rgba(0,0,0,.1);
             .img-box{
