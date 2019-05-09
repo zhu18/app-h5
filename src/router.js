8425fa159b0bdb0373aaf3032a9c5ab894c555ff
zhouzhiguo <zhiguo0508@126.com>
2019-05-09 13:34:31 +0800

    .

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import footerTab from './components/footerTab/footerTab';
-import headerBar from './components/header';
+import footerTab from './components/footerTab/footerTab'
+import headerBar from './components/header'
 
 /* ============== 主菜单 ================ */
 /* - - - - - - - 1.首页 - - - - - - - - - */
-import home from './views/home/home';
+import home from './views/home/home'
 /* - - - - - - - 2.执法 - - - - - - - - - */
 import LawEnforcement from './views/lawEnforcement'
 // 执法记录
@@ -34,7 +34,6 @@ import smartSearch from './views/smartSearch'
 import searchHistory from './views/searchResult/searchHistory'
 /* - - - - - - - 5.我的 - - - - - - - - - */
 
-
 /* ============== 首页6个模块 ================ */
 /* - - - - - - - 1.商标查询 - - - - - - - - - */
 import trademarkInquiry from './views/trademarkInquiry'
@@ -56,287 +55,332 @@ import evidenceList from './views/evidenceList'
 //我的收藏
 import selfCollection from './views/collection/self'
 /* - - - - - - - 5.新闻 - - - - - - - - - */
-import News from './views/news';
+import News from './views/news'
 //新闻详情
-import newsDetails from './views/news/newsdetails.vue';
+import newsDetails from './views/news/newsdetails.vue'
 /* - - - - - - - 6.阅读 - - - - - - - - - */
 import brandProcess from './views/brandProcess'
+/* - - - - - - - 7.我的 - - - - - - - - - */
+import my from './views/my'
+import set from './views/my/set'
+import about from './views/my/about'
 // 临时测试路由
-import my from './views/test/my'
 import test3 from './views/test/test3'
 import test2 from './views/test/test2'
 import test4 from './views/test/test4'
 
 Vue.use(Router)
 Router.prototype.goBack = function() {
-    this.isBack = true;
-    window.history.go(-1);
-};
+  this.isBack = true
+  window.history.go(-1)
+}
 export default new Router({
-    routes: [{
-            path: '/',
-            name: 'login',
-            component: () =>
-                import ('./views/login/login')
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('./views/login/login')
+    },
+    {
+      //加载首页
+      path: '/main',
+      name: 'main',
+      component: () => import('./views/home/main'),
+      children: [
+        {
+          //主页
+          path: '/home',
+          components: {
+            body: home,
+            footer: footerTab,
+            header: headerBar
+          }
         },
-        { //加载首页
-            path: '/main',
-            name: 'main',
-            component: () =>
-                import ('./views/home/main'),
-            children: [{ //主页
-                    path: '/home',
-                    components: {
-                        body: home,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                /* - - - - - - - 商标查询 - - - - - - - - - */
-                { //商标查询
-                    path: '/trademarkinquiry',
-                    name: 'trademarkinquiry',
-                    components: {
-                        body: trademarkInquiry,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                { //商标详情
-                    path: '/detailsInfo',
-                    name: 'detailsInfo',
-                    components: {
-                        body: detailsInfo,
-                    }
-                },
-                { //查询结果
-                    path: '/searchresult',
-                    name: 'searchresult',
-                    components: {
-                        body: searchresult,
-                        footer: footerTab
-                    }
-                },
-                { //查询历史
-                    path: '/searchhistory',
-                    name: 'searchhistory',
-                    components: {
-                        body: searchHistory
-                    }
-                },
-                /* - - - - - - - 智能检索 - - - - - - - - - */
-                { //智能检索
-                    path: '/smartsearch',
-                    name: 'smartsearch',
-                    components: {
-                        body: smartSearch
-                    }
-                },
-                 /* - - - - - - - 法律法规 - - - - - - - - - */
-                { //列表
-                    path: '/lawsRegulations',
-                    name: 'lawsRegulations',
-                    components: {
-                        body: lawsRegulations
-                    }
-                },
-                { //详情
-                    path: '/lawsRegulationsDetail',
-                    name: 'lawsRegulationsDetail',
-                    components: {
-                        body: lawsRegulationsDetail
-                    }
-                },
-                /* - - - - - - - 执法 - - - - - - - - - */
-                { //执法
-                    path: '/lawenforcement',
-                    name: 'lawenforcement',
-                    components: {
-                        body: LawEnforcement,
-                        footer: footerTab
-                    }
-                },
-                { //新建协同码
-                    path: '/cooperationCode',
-                    name:'cooperationCode',
-                    components: {
-                        body: cooperationCode
-                    }
-                },
-                { //执法协同码
-                    path: '/synergyCode',
-                    name:'synergyCode',
-                    components: {
-                        body: synergyCode,
-                        header: headerBar
-                    }
-                },
-                {
-                    //取证管理
-                    path: '/evidenceList',
-                    name: 'evidenceList',
-                    components: {
-                        body: evidenceList
-                    }
-                },
-                { //我的收藏
-                    path: '/selfCollection',
-                    name: 'selfCollection',
-                    components: {
-                        body: selfCollection
-                    }
-                },
-                 { //取证详情
-                     path: '/lawDetails',
-                     name: 'lawDetails',
-                     components: {
-                         body: lawDetails
-                    }
-                },
-                { //执法记录
-                    path: '/lelog',
-                    name: 'lelog',
-                    components: {
-                        body: LElog
-                    }
-                },
-               
-                { //执法记录详情
-                    path: '/enforcementdetails',
-                    name: 'enforcementdetails',
-                    components: {
-                        body: enforcementdetails
-                    }
-                },
-                { //执法地图
-                    path: '/map',
-                    name: 'map',
-                    components: {
-                        body: map
-                    }
-                },
-                { //执法日期
-                    path: '/dateSelect',
-                    name:'dateSelect',
-                    components: {
-                        body: dateSelect,
-                        footer: footerTab
-                    }
-                },
-                { //开始执法
-                    path: '/startLawEnforcement',
-                    name: 'startLawEnforcement',
-                    components: {
-                        body: startLawEnforcement
-                    }
-                },
-                { //开始执法-上传
-                    path: '/startLawEnforcementUpload',
-                    name: 'startLawEnforcementUpload',
-                    components: {
-                        body: startLawEnforcementUpload
-                    }
-                },
-                { //开始执法-执法人员列表
-                    path: '/enforcerList',
-                    name: 'enforcerList',
-                    components: {
-                        body: enforcerList
-                    }
-                },
-                /* - - - - - - - 4.收藏 - - - - - - - - - */
-                { //收藏
-                    path: '/collection',
-                    name: 'collection',
-                    redirect: '/collection/brandTab',
-                    components: {
-                        body: collection,
-                    },
-                    children:[
-                      {
-                        name:'brandTab',
-                        path:'brandTab',
-                        component:brandTab
-                      },
-                      {
-                        name:'lawTab',
-                        path:'lawTab',
-                        component:lawTab
-                      },
-                      {
-                        name:'newsTab',
-                        path:'newsTab',
-                        component:newsTab
-                      }
-                    ]
-                },
-               
-                /* - - - - - - - 5.新闻 - - - - - - - - - */
-                { //新闻
-                    path: '/news',
-                    name: 'news',
-                    components: {
-                        body: News,
-                        footer: footerTab,
-                    }
-                },
-                {//新闻详情
-                    path: '/newsDetails',
-                    name: 'newsDetails',
-                    components: {
-                        body: newsDetails,
-                        footer: footerTab,
-                    }
-                },
-            /* - - - - - - - 6.阅读 - - - - - - - - - */
-                 {//商标流程
-                    path: '/brandProcess',
-                    name: 'brandProcess',
-                    components: {
-                        body: brandProcess
-                    }
-                },
-                { //生活服务主页
-                    path: '/test2',
-                    components: {
-                        body: test2,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                { //生活服务主页
-                    path: '/test2',
-                    name: 'test2',
-                    components: {
-                        body: test2,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                { //生活服务主页
-                    path: '/test3',
-                    components: {
-                        body: test3,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                { //生活服务主页
-                    path: '/test4',
-                    components: {
-                        body: test4,
-                        footer: footerTab,
-                        header: headerBar
-                    }
-                },
-                { //生活服务主页
-                    path: '/my',
-                    components: {
-                        body: my,
-                        footer: footerTab,
-                    }
-                },
-            ]
+        /* - - - - - - - 商标查询 - - - - - - - - - */
+        {
+          //商标查询
+          path: '/trademarkinquiry',
+          name: 'trademarkinquiry',
+          components: {
+            body: trademarkInquiry,
+            footer: footerTab,
+            header: headerBar
+          }
         },
-    ]
+        {
+          //商标详情
+          path: '/detailsInfo',
+          name: 'detailsInfo',
+          components: {
+            body: detailsInfo
+          }
+        },
+        {
+          //查询结果
+          path: '/searchresult',
+          name: 'searchresult',
+          components: {
+            body: searchresult,
+            footer: footerTab
+          }
+        },
+        {
+          //查询历史
+          path: '/searchhistory',
+          name: 'searchhistory',
+          components: {
+            body: searchHistory
+          }
+        },
+        /* - - - - - - - 智能检索 - - - - - - - - - */
+        {
+          //智能检索
+          path: '/smartsearch',
+          name: 'smartsearch',
+          components: {
+            body: smartSearch
+          }
+        },
+        /* - - - - - - - 法律法规 - - - - - - - - - */
+        {
+          //列表
+          path: '/lawsRegulations',
+          name: 'lawsRegulations',
+          components: {
+            body: lawsRegulations
+          }
+        },
+        {
+          //详情
+          path: '/lawsRegulationsDetail',
+          name: 'lawsRegulationsDetail',
+          components: {
+            body: lawsRegulationsDetail
+          }
+        },
+        /* - - - - - - - 执法 - - - - - - - - - */
+        {
+          //执法
+          path: '/lawenforcement',
+          name: 'lawenforcement',
+          components: {
+            body: LawEnforcement,
+            footer: footerTab
+          }
+        },
+        {
+          //新建协同码
+          path: '/cooperationCode',
+          name: 'cooperationCode',
+          components: {
+            body: cooperationCode
+          }
+        },
+        {
+          //执法协同码
+          path: '/synergyCode',
+          name: 'synergyCode',
+          components: {
+            body: synergyCode,
+            header: headerBar
+          }
+        },
+        {
+          //取证管理
+          path: '/evidenceList',
+          name: 'evidenceList',
+          components: {
+            body: evidenceList
+          }
+        },
+        {
+          //我的收藏
+          path: '/selfCollection',
+          name: 'selfCollection',
+          components: {
+            body: selfCollection
+          }
+        },
+        {
+          //取证详情
+          path: '/lawDetails',
+          name: 'lawDetails',
+          components: {
+            body: lawDetails
+          }
+        },
+        {
+          //执法记录
+          path: '/lelog',
+          name: 'lelog',
+          components: {
+            body: LElog
+          }
+        },
+
+        {
+          //执法记录详情
+          path: '/enforcementdetails',
+          name: 'enforcementdetails',
+          components: {
+            body: enforcementdetails
+          }
+        },
+        {
+          //执法地图
+          path: '/map',
+          name: 'map',
+          components: {
+            body: map
+          }
+        },
+        {
+          //执法日期
+          path: '/dateSelect',
+          name: 'dateSelect',
+          components: {
+            body: dateSelect,
+            footer: footerTab
+          }
+        },
+        {
+          //开始执法
+          path: '/startLawEnforcement',
+          name: 'startLawEnforcement',
+          components: {
+            body: startLawEnforcement
+          }
+        },
+        {
+          //开始执法-上传
+          path: '/startLawEnforcementUpload',
+          name: 'startLawEnforcementUpload',
+          components: {
+            body: startLawEnforcementUpload
+          }
+        },
+        {
+          //开始执法-执法人员列表
+          path: '/enforcerList',
+          name: 'enforcerList',
+          components: {
+            body: enforcerList
+          }
+        },
+        /* - - - - - - - 4.收藏 - - - - - - - - - */
+        {
+          //收藏
+          path: '/collection',
+          name: 'collection',
+          redirect: '/collection/brandTab',
+          components: {
+            body: collection
+          },
+          children: [
+            {
+              name: 'brandTab',
+              path: 'brandTab',
+              component: brandTab
+            },
+            {
+              name: 'lawTab',
+              path: 'lawTab',
+              component: lawTab
+            },
+            {
+              name: 'newsTab',
+              path: 'newsTab',
+              component: newsTab
+            }
+          ]
+        },
+
+        /* - - - - - - - 5.新闻 - - - - - - - - - */
+        {
+          //新闻
+          path: '/news',
+          name: 'news',
+          components: {
+            body: News,
+            footer: footerTab
+          }
+        },
+        {
+          //新闻详情
+          path: '/newsDetails',
+          name: 'newsDetails',
+          components: {
+            body: newsDetails,
+            footer: footerTab
+          }
+        },
+        /* - - - - - - - 6.阅读 - - - - - - - - - */
+        {
+          //商标流程
+          path: '/brandProcess',
+          name: 'brandProcess',
+          components: {
+            body: brandProcess
+          }
+        },
+        {
+          //生活服务主页
+          path: '/test2',
+          components: {
+            body: test2,
+            footer: footerTab,
+            header: headerBar
+          }
+        },
+        {
+          //生活服务主页
+          path: '/test2',
+          name: 'test2',
+          components: {
+            body: test2,
+            footer: footerTab,
+            header: headerBar
+          }
+        },
+        {
+          //生活服务主页
+          path: '/test3',
+          components: {
+            body: test3,
+            footer: footerTab,
+            header: headerBar
+          }
+        },
+        {
+          //生活服务主页
+          path: '/test4',
+          components: {
+            body: test4,
+            footer: footerTab,
+            header: headerBar
+          }
+        },
+        {
+          //我的
+          path: '/my',
+          components: {
+            body: my,
+            footer: footerTab
+          }
+        },
+        {
+          //设置
+          path: '/set',
+          name: 'set',
+          components: { body: set }
+        },
+        {
+          //设置
+          path: '/about',
+          name: 'about',
+          components: { body: about }
+        }
+      ]
+    }
+  ]
 })
---
+++ src/views/my/set.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="wrap">
+    <div class="top-header">
+      <span class="iconfont icon-back" @click="back"></span>
+      <span class="title">设置</span>
+      <span class="iconfont-k"></span>
+    </div>
+    <p class="font-size">字体大小</p>
+    <div class="panel">
+      这是一段测试文字，会按照下方您的选择调整字体大小
+    </div>
+    <ul class="handle-panel">
+      <li class="samll panel-item"></li>
+      <li class="middle panel-item"></li>
+      <li class="big panel-item"></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    back() {
+      this.$router.goBack()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+// ::-webkit-search-cancel-button {
+//   display: none;
+// }
+.wrap {
+  .top-header {
+    height: 0.9rem;
+    background-color: #2095f2;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
+    .iconfont-k {
+      width: 0.64rem;
+      display: inline-block;
+      visibility: hidden;
+    }
+    .iconfont {
+      font-size: 0.84rem;
+      color: #fff;
+    }
+    .title {
+      font-size: 0.36rem;
+    }
+  }
+  .font-size {
+    font-size: 0.28rem;
+    line-height: 0.8rem;
+    height: 0.57rem;
+    padding-left: 0.33rem;
+  }
+  .panel {
+    top: 3.7rem;
+    font-size: 0.32rem;
+    height: 3.3rem;
+    width: 6.86rem;
+    margin: 0.33rem;
+    background: #fff;
+    border-radius: 0.1rem;
+    box-shadow: 0 0px 10px 1px rgba(4, 0, 0, 0.1);
+    padding: 0.8rem 0.32rem;
+    box-sizing: border-box;
+    line-height: .5rem;
+  }
+  .handle-panel {
+    position: fixed;
+    bottom: 2.8rem;
+    height: 2rem;
+    width: 6.86rem;
+    margin: 0.33rem;
+    background: #fff;
+    border-radius: 0.1rem;
+    box-shadow: 0 0px 10px 1px rgba(4, 0, 0, 0.1);
+    padding: 0.8rem 0.32rem;
+    box-sizing: border-box;
+    .panel-item{
+      float: left;
+      width: 0.4rem;
+      height: 0.4rem;
+      border-radius:50%;
+      border: 1px solid #bfbfbf;
+      padding:  0.04rem;
+      background: #bfbfbf;
+      background-clip: content-box;
+      box-sizing:border-box; 
+      position: relative;
+      text-align: center;
+      &:nth-child(1){
+        margin-left: .8rem;
+        margin-right: 1.85rem;
+        &::before{
+          content: '小';
+          position: absolute;
+          width: 0.4rem;
+          height: 0.6rem;
+          line-height: .6rem;
+          top:-.6rem;
+           left: 0;
+          font-size: .28rem;
+        }
+      }
+      &:nth-child(2){
+        margin-right: 1.85rem;
+         &::before{
+          content: '中';
+          position: absolute;
+          width: 0.4rem;
+          height: 0.6rem;
+          line-height: .6rem;
+          top:-.6rem;
+          left: 0;
+          font-size: .32rem;
+        }
+      }
+      &:nth-child(3){
+         &::before{
+          content: '大';
+          position: absolute;
+          width: 0.4rem;
+          height: 0.6rem;
+          line-height: .6rem;
+          top:-.6rem;
+           left: 0;
+          font-size: .4rem;
+        }
+      }
+    }
+  }
+}
+</style>
